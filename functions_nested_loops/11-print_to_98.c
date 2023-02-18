7c9e910e7d154db1f83b53302190d19e829891fe
Faqu22 <124268857+Faqu22@users.noreply.github.com>
2023-02-18 14:34:54 -0300

    Update 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,39 +1,38 @@
 #include "main.h"
-void print_to_98(int n)
+void
+print_to_98(int n)
 {
-while(n != 98)
-    {
-        if(n < 0)
+        while (n != 98)
         {
-            putchar(45);
-            if(n < -9)
-            {  
-                putchar((n * -1 % 100 / 10) + '0');
-            }
-            putchar((n * -1 % 10) + '0');
+                if (n < 0)
+                {
+                        _putchar(45);
+                        if (n < -9)
+                        {
+                                _putchar((n * -1 % 100 / 10) + '0');
+                        }
+                        _putchar((n * -1 % 10) + '0');
+                }
+                if (n >= 0 && n <= 9)
+                        _putchar(n + '0');
+                if (n > 9)
+                {
+                        if (n > 99)
+                                _putchar((n / 100) + '0');
+                        _putchar((n % 100 / 10) + '0');
+                        _putchar((n % 10) + '0');
+                }
+                if (n != 98)
+                {
+                        _putchar(44);
+                        _putchar(32);
+                }
+                n = n < 98 ? n + 1 : n - 1;
         }
-            
-        if(n >= 0 && n <= 9)
-            putchar(n + '0');
-            
-        if(n > 9)
+        if (n == 98)
         {
-            if(n > 99)
-            {
-                putchar((n / 100) + '0');
-            }
-            putchar((n % 100 / 10) + '0');
-            putchar((n % 10) + '0');
+                _putchar(57);
+                _putchar(56);
+                _putchar(10);
         }
-        if(n != 98)
-        {
-            putchar(44);
-            putchar(32);
-        }
-        n = n < 98 ? ++n : --n;
-    }
-    if (n == 98)
-    {   putchar(57);
-        putchar(56);
-    }
 }

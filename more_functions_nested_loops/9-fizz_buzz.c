36f71c4c392b46bc47b5bb287c56d17c9ef53bc5
Faqu22 <facundoalvarez415@gmail.com>
2023-02-22 10:23:47 -0800

    Task 9 Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -10,12 +10,13 @@ main(void)
 		printf("FizzBuzz ");
 		if ((i % 3 == 0) && (i % 5 != 0))
 			printf("Fizz ");
-		if ((i % 5 == 0) && ((i % 3 !=0) && (i != 99)))
+		if ((i % 5 == 0) && ((i % 3 != 0) && (i != 100)))
 			printf("Buzz ");
 		if ((i % 3 != 0) && (i % 5 != 0))
 			printf("%d ", i);
-		if(i == 99)
+		if (i == 100)
 			printf("Buzz");
 	}
+	printf("\n");
 	return (0);
 }

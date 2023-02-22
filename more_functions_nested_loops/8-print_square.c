b6a545d4afd64e9b5c00b8d9ca432e64d49517d6
Faqu22 <facundoalvarez415@gmail.com>
2023-02-22 09:50:08 -0800

    Task 8 You are so much sunshine in every square inch

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+void
+print_square(int size)
+{
+	int i;
+	int a;
+
+	for (i = 0; i < size ; i++)
+	{
+		a = 0;
+		while (a < size)
+		{
+			_putchar(35);
+			a++;
+		}
+	}
+	if (size < 1)
+		_putchar(10);
+}

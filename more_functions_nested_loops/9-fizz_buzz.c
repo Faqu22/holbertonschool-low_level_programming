dccbcba264f87f0334c096fb53d4fa3d8a7b3862
Faqu22 <facundoalvarez415@gmail.com>
2023-02-22 10:13:49 -0800

    Task 9 Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,13 +4,13 @@ main(void)
 {
 	int i;
 
-	for (i = 1; i < 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3 == 0) && ((i % 5 == 0) && (i != 100)))
 		printf("FizzBuzz ");
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 5 != 0))
 			printf("Fizz ");
-		if (i % 5 == 0)
+		if ((i % 5 == 0) && (i % 3 !=0))
 			printf("Buzz ");
 		if ((i % 3 != 0) && (i % 5 != 0))
 			printf("%d ", i);

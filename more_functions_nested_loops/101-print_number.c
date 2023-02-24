b6534a276d89e4b783ba5b8dbe146e108d90a6d7
Faqu22 <facundoalvarez415@gmail.com>
2023-02-24 07:50:18 -0800

    Task 12 Numbers have life; they're not just symbols on paper

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <limits.h>
+void
+print_number(int n)
+{
+	int div, ult, zero;
+	int mod = 10;
+	int cont = 0;
+	int neg = n;
+
+	if (n < 0)
+	{
+		ult = n % 10 * -1;
+		n = n / 10 * -1;
+		_putchar('-');
+	}
+	while (n > 9)
+	{
+		while (mod < n)
+		{
+			mod = mod * 10;
+			cont = 0;
+		}
+		while (mod > n)
+		{
+			mod = mod / 10;
+			cont++;
+		}
+		div = n;
+		while (div > 9)
+		{
+			div = div / 10;
+		}
+		zero = cont - 1;
+		if (cont != 1)
+		{
+			while (zero > 1)
+			{
+				_putchar('0');
+				zero--;
+			}
+		}
+		_putchar(div + '0');
+		n = n % mod;
+	}
+	_putchar(n + '0');
+	if (neg < 0)
+		_putchar(ult + '0');
+}

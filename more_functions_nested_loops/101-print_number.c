20a8b1b22cf76cf6ccb7ecfab0455acc9dd13655
Faqu22 <facundoalvarez415@gmail.com>
2023-02-25 09:52:41 -0800

    Task 12 Numbers have life; they're not just symbols on paper

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -4,9 +4,9 @@ void
 print_number(int n)
 {
 	int first, numNew = n, ult, div = 1, contador = 0;
-	(n == INT_MIN) ? ult = n % 10 * -1, n = n / 10 * -1, putchar('-') : -1;
-	(n < 0) ? n = n * -1, putchar('-') : -1;
-	(n == 0) ? putchar('0') : -1;
+	(n == INT_MIN) ? ult = n % 10 * -1, n = n / 10 * -1, _putchar('-') : -1;
+	(n < 0) ? n = n * -1, _putchar('-') : -1;
+	(n == 0) ? _putchar('0') : -1;
 	while (n > 9)
 	{
 		while (div < n)
@@ -25,11 +25,11 @@ print_number(int n)
 		}
 		while (contador > 1)
 		{
-			putchar('0');
+			_putchar('0');
 			contador--;
 		}
 		(contador == 1) ? contador = 0 : -1;
-		(n > 9) ? first = n / div, putchar(first + '0'), n = n % div : -1;
+		(n > 9) ? first = n / div, _putchar(first + '0'), n = n % div : -1;
 	}
 	contador = 0;
 	while (div > n)
@@ -39,9 +39,9 @@ print_number(int n)
 	}
 	while (contador > 1)
 	{
-		putchar('0');
+		_putchar('0');
 		contador--;
 	}
-	(n == 0) ? -1 : putchar(n + '0');
-	(numNew == INT_MIN) ? putchar(ult + '0') : -1;
+	(n == 0) ? -1 : _putchar(n + '0');
+	(numNew == INT_MIN) ? _putchar(ult + '0') : -1;
 }

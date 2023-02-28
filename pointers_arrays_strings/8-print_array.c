76fbc31c9194d35c2ea69eb901c12ea754e1d2ea
Faqu22 <facundoalvarez415@gmail.com>
2023-02-28 03:38:53 -0800

    Task 8 Arrays are not pointers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+void
+print_array(int *a, int n)
+{
+	int cont = 0;
+
+	while (n >= cont)
+	{
+		printf("%d, ", a[cont]);
+		cont++;
+		if (cont == n - 1)
+		{
+			printf("%d\n", a[cont]);
+			break;
+		}
+	}
+}

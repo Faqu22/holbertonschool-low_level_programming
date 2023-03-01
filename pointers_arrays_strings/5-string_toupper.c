f871665acf36b306ee275cfc2f44b9de1093f548
Faqu22 <facundoalvarez415@gmail.com>
2023-03-01 07:07:54 -0800

    Task 5 Always look up

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+char
+*string_toupper(char *upp)
+{
+	int cont = 0;
+
+	while (upp[cont])
+	{
+		if (upp[cont] >= 98 && upp[cont] <= 122)
+			upp[cont] = upp[cont] - 32;
+		cont++;
+	}
+	return (upp);
+}

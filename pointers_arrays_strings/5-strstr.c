6037d84e9356f359fd43e8a3df502ec8fc8a7f02
Faqu22 <facundoalvarez415@gmail.com>
2023-03-01 18:08:26 -0800

    Task 5 strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+char *_strstr(char *haystack, char *needle)
+{
+	int cont = 0;
+	int contEnd = 0;
+	int temp = 0;
+	while (haystack[cont])
+	{
+		while(haystack[cont] == needle[temp])
+		{
+			if (temp == 0)
+				contEnd = cont;
+			cont++;
+			temp++;
+			if (needle[temp] == '\0')
+			{
+					return (haystack + contEnd);
+			}
+		}
+		cont++;
+	}
+	return(0);
+}

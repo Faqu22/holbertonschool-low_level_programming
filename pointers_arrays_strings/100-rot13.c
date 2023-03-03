15cbebc13d9925b681cbc5796600a2ea51f44527
Faqu22 <facundoalvarez415@gmail.com>
2023-03-03 13:08:30 -0800

    Task 8 rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+char *rot13(char *rot)
+{
+	char *rot13 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *tr = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int cont = 0;
+	int contTry = 0;
+
+	while (rot[cont])
+	{
+		for (contTry = 0; rot13[contTry]; contTry++)
+			{
+				if (rot[cont] == rot13[contTry])
+					rot[cont] = tr[contTry];
+			}
+		cont++;
+	}
+	return (rot);
+}

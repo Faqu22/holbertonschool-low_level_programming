9f842fe1bb4704bafd0ba871d76eb2bfaa61ce35
Faqu22 <facundoalvarez415@gmail.com>
2023-03-07 05:42:34 -0800

    Task 0  It ain't what they call you, it's what you answer to

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[])
+int
+main(int argc, char *argv[])
 {
-  int count;
+	int count;
 
-  printf ("%s\n",argv[0]);
-
-  if (argc > 1)
-    {
-      for (count = 1; count < argc; count++)
-	{
-	  printf("argv[%d] = %s\n", count, argv[count]);
-	}
-    }
-
-  return 0;
+	printf("%s\n", argv[0]);
+	if (argc > 1)
+		for (count = 1; count < argc; count++)
+			printf("argv[%d] = %s\n", count, argv[count]);
+	return (0);
 }
 

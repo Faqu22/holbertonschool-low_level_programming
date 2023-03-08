1264fe1ab56efc1cd0241eca0ba26a1be03a91ad
Faqu22 <facundoalvarez415@gmail.com>
2023-03-08 05:05:04 -0800

    Task 2 The best argument against democracy is a five-minute conversation with the average voter

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -3,14 +3,12 @@
 int
 main(int argc, char *argv[])
 {
-	int count = 1;
+	int count = 0;
 
 	while (argv[count] &&  count < argc)
 	{
 		printf("%s\n", argv[count]);
 		count++;
 	}
-	if (argc < 2)
-		printf("%s\n", argv[0]);
 	return (0);
 }

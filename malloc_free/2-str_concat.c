8d75a8e6ad584eeb1b4784c38399b5180b63e88c
Faqu22 <facundoalvarez415@gmail.com>
2023-03-14 07:21:49 -0700

    Task 2 He who is not courageous enough to take risks will accomplish nothing in life

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 char
 *str_concat(char *s1, char *s2)
 {
@@ -9,27 +10,35 @@ char
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
-	if (s1 == NULL)
-		return (s2);
-	if (s2 == NULL)
-		return (s1);
-	while (s1[cont])
-		cont++;
-	while (s2[conts2])
-		conts2++;
+	if (s1 != NULL)
+	{
+		while (s1[cont])
+			cont++;
+	}
+	if (s2 != NULL)
+	{
+		while (s2[conts2])
+			conts2++;
+	}
 	p = malloc(sizeof(char) * (cont + conts2) + 1);
 	cont = 0;
 	conts2 = 0;
-	while (s1[cont])
+	if (s1 != NULL)
 	{
-		p[cont] = s1[cont];
-		cont++;
+		while (s1[cont])
+		{
+			p[cont] = s1[cont];
+			cont++;
+		}
 	}
-	while (s2[conts2])
-	{
-		p[cont] = s2[conts2];
-		conts2++;
-		cont++;
+	if (s2 != NULL)
+	{	
+		while (s2[conts2])
+		{
+			p[cont] = s2[conts2];
+			conts2++;
+			cont++;
+		}
 	}
 	return (p);
 

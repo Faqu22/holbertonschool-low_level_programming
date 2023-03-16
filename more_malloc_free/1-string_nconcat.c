6ec71bc2cfca26e6f188c43b1e4bec1d2b3ec934
Faqu22 <facundoalvarez415@gmail.com>
2023-03-16 08:22:59 -0700

    Task 1 string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,30 +7,37 @@ char
 	char *p;
 
 	if (b < 1)
-		exit(98);
+		return (NULL);
 	p = malloc(b);
 	if (p == NULL)
-		exit(98);
+		return (NULL);
 	return (p);
 }
 
 char
 *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int cont = 0, conts2 = 0;
+	unsigned int cont = 0, conts2 = 0;
 	char *c;
 
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+
 	for (cont = 0; s1[cont] && s1 != NULL; cont++)
 		;
+
+	for (conts2 = 0; s2[conts2]; conts2++)
+		;
+
+	n = (conts2 < n) ? conts2 : n;
 	c = malloc_check(sizeof(char) * (cont + n));
+
 	for (cont = 0; s1[cont]; c[cont] = s1[cont], cont++)
 		;
-	for (conts2 = 0; s2[conts2] != s2[n] && s2[conts2]; conts2++)
+
+	for (conts2 = 0; conts2 < n; conts2++)
 		c[conts2 + cont] = s2[conts2];
+
+	c[conts2 + cont] = '\0';
 	return (c);
-
-
-
 }

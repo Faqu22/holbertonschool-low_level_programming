d4421c3241ec069122657ea5990938dcbf399074
Faqu22 <facundoalvarez415@gmail.com>
2023-03-21 13:37:34 -0700

    Task 1 A dog is the only thing on earth that loves you more than you love yourself

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include "stdio.h"
+
 /**
  * init_dog- Params for dog.
  * @d: Name of var of struct.
@@ -8,6 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

d0d1618c05942d8ceda1f4b2de85db5bbf131b6d
Faqu22 <facundoalvarez415@gmail.com>
2023-03-21 15:34:30 -0700

    Task 4 A door is what a dog is perpetually on the wrong side of

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,31 +12,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
-	long unsigned int cont;
+	unsigned int cont;
 
-	my_dog = malloc(sizeof(struct dog));
+	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
 	{
+		return (NULL);
+	}
+	my_dog->name = malloc(strlen(name) + 1);
+	if (my_dog->name == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	my_dog->owner = malloc(strlen(owner) + 1);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->owner);
+		free(my_dog->name);
 		free(my_dog);
 		return (NULL);
 	}
 	for (cont = 0; cont < strlen(name); cont++)
 	{
 		my_dog->name[cont] = name[cont];
-		if (my_dog == NULL)
-		{
-			free(my_dog);
-			return (NULL);
-		}
 	}
 	for (cont = 0; cont < strlen(owner); cont++)
 	{
 		my_dog->owner[cont] = owner[cont];
-		if (my_dog == NULL)
-		{
-			free(my_dog);
-			return (NULL);
-		}
 	}
 	my_dog->age = age;
 	return (my_dog);

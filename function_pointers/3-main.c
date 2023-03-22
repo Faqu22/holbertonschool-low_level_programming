b3a25ac34c67502e7bb5fdbfcf0158241091f19b
Faqu22 <facundoalvarez415@gmail.com>
2023-03-21 20:51:19 -0700

    Task 3 A goal is not always meant to be reached, it often serves simply as something to aim at

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -18,10 +18,15 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[1] != NULL)
-		a = atoi(argv[1]);
-	if (argv[3] != NULL)
+	if (argv[1] != NULL && argv[3] != NULL)
+	{	a = atoi(argv[1]);
 		b = atoi(argv[3]);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	printf("%d\n", ((*get_op_func(argv[2]))(a, b)));
 	return (0);
 }

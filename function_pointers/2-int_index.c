51a11111dddf01c12db3b94279fe4b59de123d7a
Faqu22 <facundoalvarez415@gmail.com>
2023-03-21 17:38:53 -0700

    Task 2  To hell with circumstances; I create opportunities

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,32 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for an match with the function.
+ * @array: Data structure.
+ * @size: Size of the array.
+ * @cmp: Function passed by parameters.
+ * Return: Return the number of elements passed until the match is found.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int cont, res = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	for (cont = 0; cont < size; cont++)
+	{
+		if ((*cmp)(array[cont]) == 0)
+			res++;
+		if ((*cmp)(array[cont]) != 0)
+			break;
+	}
+	if (res == 0)
+		return (-1);
+
+	return (res);
+}

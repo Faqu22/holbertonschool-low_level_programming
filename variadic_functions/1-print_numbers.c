73ad8183072f680108f549c6fe16f8d3a86731f8
Faqu22 <facundoalvarez415@gmail.com>
2023-03-22 19:39:50 -0700

    Task 1 To be is to be the value of a variable

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - prints the numbers dated.
+ * @separator: is a separator what include ", ".
+ * @n: numbers dated.
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int cont;
+
+	va_start(list, n);
+	for (cont = 0; cont < n; cont++)
+	{
+		if (cont != n - 1)
+			printf("%i%s", va_arg(list, const unsigned int), separator);
+	}
+	printf("%i\n", va_arg(list, const unsigned int));
+
+}

85963b7707d730528958a229c0a0492050d6f51b
Faqu22 <facundoalvarez415@gmail.com>
2023-03-22 20:00:17 -0700

    Task 2 One woman's constant is another woman's variable

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - prints the strings.
+ * @separator: is a separator what include ", ".
+ * @n: strings dated.
+ *
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int cont;
+
+	va_start(list, n);
+	if (separator == NULL)
+		separator = "";
+	for (cont = 0; cont < n; cont++)
+	{
+		if (cont != n - 1)
+			printf("%s%s", va_arg(list, char *), separator);
+	}
+	if (n != 0)
+		printf("%s", va_arg(list, char *));
+	printf("\n");
+	va_end(list);
+}

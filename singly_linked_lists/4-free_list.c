0aefffe0c74bea41be0cdebebd633cd0463bdbe4
Faqu22 <facundoalvarez415@gmail.com>
2023-04-03 15:19:22 -0700

    Task 4 Free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,9 +7,17 @@
  */
 void free_list(list_t *head)
 {
+	struct list_s *temp;
+
 	if (head == NULL)
 		return;
-	free(head->str);
+	while(head->next != NULL)
+	{
+		free(head->str);
+		temp = head->next;
+		head = head->next;
+		free(&temp);
+	}
 	free(head->next);
 	free(head);
 }

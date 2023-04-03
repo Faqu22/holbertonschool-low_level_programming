08d8cf02b2ceba979fd783444091d9929bef2c69
Faqu22 <facundoalvarez415@gmail.com>
2023-04-03 16:53:10 -0700

    Task 2 Add node

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -17,7 +17,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->prev = new;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
 	new->next = *head;
 	
 	*head = new;

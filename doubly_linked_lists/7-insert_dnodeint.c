db1fc92cd8fb6f88503f1c13419e7811d836077a
Faqu22 <facundoalvarez415@gmail.com>
2023-04-03 20:51:47 -0700

    Task 7  Insert at index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,6 +29,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			break;
 		}
 		*h = (*h)->next;
+		if ((*h)->next == NULL)
+		{
+			free(new);
+			return (NULL);
+		}
 		i++;
 	}
 	new->n = n;

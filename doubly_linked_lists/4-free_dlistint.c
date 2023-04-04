59f2818bb535b9d54bf75ee7005e5ea79172425e
Faqu22 <facundoalvarez415@gmail.com>
2023-04-03 18:02:45 -0700

    Task 4 Free list

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -7,13 +7,13 @@
  */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *current = head;
-    dlistint_t *temp;
+	dlistint_t *current = head;
+	dlistint_t *temp;
 
-    while (current != NULL)
-    {
-        temp = current;
-        current = current->next;
-        free(temp);
-    }
+	while (current != NULL)
+	{
+		temp = current;
+		current = current->next;
+		free(temp);
+	}
 }

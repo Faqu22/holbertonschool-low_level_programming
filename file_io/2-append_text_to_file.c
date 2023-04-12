9b17267958b80b7c89ee3217520938eeda110aaa
Faqu22 <facundoalvarez415@gmail.com>
2023-04-12 06:43:31 -0700

    Task 2 Speak gently, she can hear

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -13,7 +13,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, cont;
 
-	if (!*filename)
+	if (!*filename || filename == NULL)
 		return (-1);
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)

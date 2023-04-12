14a1f86a203143c05ed921f47a5e81ddf8e26cc7
Faqu22 <facundoalvarez415@gmail.com>
2023-04-12 06:13:20 -0700

    Task 1 Under the snow

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,6 +16,8 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_TRUNC | O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
+	if (!*text_content)
+		text_content = "";
 	for (cont = 0; text_content[cont]; cont++)
 		;
 	if (cont != 0)

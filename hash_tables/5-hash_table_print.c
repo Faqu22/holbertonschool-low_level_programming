dd92a7c79578e5d46dc0cc2400de02472276ea41
Faqu22 <facundoalvarez415@gmail.com>
2023-04-15 13:51:59 -0700

    Task 5 >>> print(ht)

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print all the hash table.
+ *
+ * @ht: the hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *p;
+	int n = 0;
+
+	printf("{");
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p)
+		{
+			if (n != 0)
+				printf(", ");
+			printf("'%s': '%s'", p->key, p->value);
+			p = p->next;
+			n = 1;
+		}
+	}
+	printf("}\n");
+}
+

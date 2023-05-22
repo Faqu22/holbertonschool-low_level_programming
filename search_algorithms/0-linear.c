72df584082b6519d7258a463af8e2374ea11d8a7
Faqu22 <facundoalvarez415@gmail.com>
2023-05-22 18:14:41 -0300

    Tasks

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,15 +3,13 @@
 int
 linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
+	size_t i = 0;
 
-
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%li] = [%i]\n", i, array[i]);    
-        if (array[i] == value)
-            break;
-    }
-
-    return ((i < size) ? (int)i : -1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			break;
+	}
+	return ((i < size) ? (int)i : -1);
 }
